7e521e76b0524402b027710e3a7a8bc8e0a24f6c
jimbonica <jim@moneyexperience.com>
2020-07-09 11:24:53 -0400

    addition of step handling User Agreement and first version of new feature files of new Essentials and Professional content

---
+++ src/test/java/runner/F_1004_CompleteSimulatorEssentials7_Test.java
@@ -0,0 +1,17 @@
+package runner;
+
+import io.cucumber.junit.Cucumber;
+import org.junit.runner.RunWith;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		//tags = {""},
+	//	strict = true,
+		plugin = { "pretty", "html:target/html/1002_Complete_Simulator_Essentials6/",
+			"json:target/json/1002_Complete_Simulator_Essentials6.json" }, 
+		features = {"src/test/resources/cucumber/1002_Complete_Simulator_Essentials6.feature"},
+		glue = {"/"},
+		monochrome = true)
+public class F_1004_CompleteSimulatorEssentials7_Test {
+}
